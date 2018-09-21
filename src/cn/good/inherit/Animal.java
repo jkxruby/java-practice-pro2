d00ae7266203b172a047262c77ee9067944f8f27
jkxruby <jkxruby@gmail.com>
2018-09-21 11:09:04 +0800

    test inherit

---
+++ src/cn/good/inherit/Animal.java
@@ -0,0 +1,61 @@
+package cn.good.inherit;
+
+/**
+ * Created by jkx on 2018/9/21.
+ */
+public class Animal {
+    String eye; //属性
+
+    public void run(){    //方法
+        System.out.println("run like wind");
+
+    }
+
+    public void eat(){
+        System.out.println("eat food");
+    }
+
+
+}
+
+class Mammal extends Animal{  //哺乳动物
+    String eye;
+
+    public void run(){
+        System.out.println("我的跑法不一样");
+
+    }
+
+    public void eat(){
+        System.out.println("我的吃饭");
+    }
+    public void taisheng(){
+        System.out.println("胎生动物");
+    }
+
+
+    public static void main(String[] args){
+        Paxing pa = new Paxing();
+        pa.eat();
+        pa.eye = "hah";
+        pa.run();
+        pa.nuansheng();
+
+    }
+
+}
+
+
+class Paxing extends Animal{
+    String eye;
+
+    public void run(){
+        System.out.println("paxing run");
+    }
+
+
+    public void nuansheng(){
+        System.out.println("wo shi nuan sheng");
+    }
+}
+
---
+++ src/cn/good/oo/User.java
@@ -0,0 +1,11 @@
+package cn.good.oo;  //必须是非注释语句的第一句，第一句
+import java.lang.String;
+
+/**
+ * Created by jkx on 2018/9/20.
+ */
+public class User {
+
+    String name;
+    int id;
+}
